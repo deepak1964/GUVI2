3307f07daf026ec2c5d143a0a6d1a222e6004b87
deepak1964 <127740099+deepak1964@users.noreply.github.com>
2025-02-05 10:34:52 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
     echo hi123
     sh 'chmod +x build.sh'
     sh './build.sh'
-    docker login -u deepak102 -p 22ISR008 
+    docker login -u deepak102 -p deepak@2005
     docker tag test deepak102/task2
     docker push deepak102/task2
     
